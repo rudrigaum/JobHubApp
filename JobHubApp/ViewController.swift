df60f8aef6b810191564a8806e12e7205a823c30
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2025-07-10 17:16:22 -0300

    Initial Commit

---
+++ JobHubApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JobHubApp
+//
+//  Created by Rodrigo Cerqueira Reis on 10/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
